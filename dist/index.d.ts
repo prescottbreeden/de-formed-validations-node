b3f395a63ac148d037b95ffe18a59e94c469f574
bobross <androgenagonist@gmail.com>
2021-09-13 01:57:10 -0700

    updated tarball

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,15 +1,2 @@
-import { ValidationSchema } from '@de-formed/base';
-export declare function Validation<S>(validationSchema: ValidationSchema<S>): {
-    getAllErrors: any;
-    getError: any;
-    getFieldValid: any;
-    isValid: null;
-    resetValidationState: () => void;
-    setValidationState: any;
-    validate: any;
-    validateAll: any;
-    validateAllIfTrue: any;
-    validateIfTrue: any;
-    validationErrors: null;
-    validationState: null;
-};
+import { ValidationSchema, BaseValidation } from '@de-formed/base';
+export declare function Validation<S>(validationSchema: ValidationSchema<S>): BaseValidation<S>;
--- dist/index.js
+++ dist/index.js
@@ -1,56 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Validation = void 0;
-const utilities_1 = require("./utilities");
 const base_1 = require("@de-formed/base");
 const useCache = (initial) => {
     let value = initial;
-    const setValue = (data) => {
+    const setValidationState = (data) => {
         value = data;
         return data;
     };
-    const retrieveValue = () => value;
-    return [retrieveValue, setValue];
+    const getValidationState = () => value;
+    return { getValidationState, setValidationState };
 };
 function Validation(validationSchema) {
-    const [getValidationState, setValidationState] = (0, utilities_1.compose)(useCache, base_1.createValidationState)(validationSchema);
-    const resetValidationState = () => {
-        return (0, utilities_1.compose)(setValidationState, base_1.createValidationState)(validationSchema);
-    };
-    const validate = (0, base_1.createValidate)(validationSchema, getValidationState, setValidationState);
-    const validateAll = (0, base_1.createValidateAll)(validationSchema, getValidationState, setValidationState);
-    const validateAllIfTrue = (0, base_1.createValidateAllIfTrue)(validationSchema, getValidationState, setValidationState);
-    const validateIfTrue = (0, base_1.createValidateIfTrue)(validationSchema, getValidationState, setValidationState);
-    const getError = (0, base_1.createGetError)(getValidationState);
-    const getAllErrors = (0, base_1.createGetAllErrors)(getValidationState);
-    const getFieldValid = (0, base_1.createGetFieldValid)(getValidationState);
-    const validationObject = {
-        getAllErrors,
-        getError,
-        getFieldValid,
-        isValid: null,
-        resetValidationState,
-        setValidationState,
-        validate,
-        validateAll,
-        validateAllIfTrue,
-        validateIfTrue,
-        validationErrors: null,
-        validationState: null,
-    };
-    Object.defineProperty(validationObject, 'isValid', {
-        get: () => (0, base_1.calculateIsValid)(getValidationState),
-        enumerable: true,
-    });
-    Object.defineProperty(validationObject, 'validationState', {
-        get: getValidationState,
-        enumerable: true,
-    });
-    Object.defineProperty(validationObject, 'validationErrors', {
-        get: () => (0, base_1.gatherValidationErrors)(getValidationState),
-        enumerable: true,
-    });
-    return validationObject;
+    const { getValidationState, setValidationState } = useCache((0, base_1.createValidationState)(validationSchema));
+    return new base_1.BaseValidation(validationSchema, getValidationState, setValidationState);
 }
 exports.Validation = Validation;
 //# sourceMappingURL=index.js.map
